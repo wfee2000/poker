20b63c594f39a72e32849a8afc992b49db34e78f
VeQox <sonnleitner.luki@gmail.com>
2023-06-06 20:11:14 +0200

    restructure app

---
+++ poker-frontend/src/routes/(app)/+layout.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+    import Logo from "$lib/logo.svelte";
+</script>
+
+<div class="grid items-center justify-around grid-cols-3 bg-onedark-darkblue">
+    <a href="/" class="p-2">
+        <Logo/>
+    </a>
+    <a href="/rooms" class="font-mono text-2xl text-center">Rooms</a>
+    <a href="/auth/login" class="font-mono text-2xl text-center">Login</a>
+</div>
+
+<slot></slot>
--- poker-frontend/src/routes/+layout.svelte
+++ poker-frontend/src/routes/+layout.svelte
@@ -1,17 +1,9 @@
 <script lang="ts">
-    import "../app.css";
-    import Logo from "$lib/logo.svelte";
+    import "../app.css"
 </script>
 
-<div class="grid items-center justify-around grid-cols-3 bg-onedark-darkblue">
-    <a href="/" class="p-2">
-        <Logo/>
-    </a>
-    <a href="/rooms" class="font-mono text-2xl text-center">Rooms</a>
-    <a href="/auth/login" class="font-mono text-2xl text-center">Login</a>
-</div>
-
 <slot></slot>
+
 
 <style lang="postcss">
     :global(html) {
