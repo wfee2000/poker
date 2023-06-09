69be310f0eee5acb5d17f61781f4e7d9d237b416
ICQun <jln.schludermann@gmail.com>
2023-06-09 13:15:11 +0200

    feat: evaluate winner
    f**k this cost me my last braincell

--- poker-backend/src/game/card.ts
+++ poker-backend/src/game/card.ts
@@ -46,280 +46,170 @@ export const Deck: Card[] = cartesian(Object.values(CardColor), CardValues).map(
 
 export class PokerEvaluate {
     public static evalWinners(players: Player[], communityCards: Card[]): Player[] {
-        let evaluation: Map<string, [PokerHand, number][]> = new Map<string, [PokerHand, number][]>();
+        let evaluation: Map<Player, [PokerHand, number[]][]> = new Map<Player, [PokerHand, number[]][]>();
 
         for (let pl of players) {
+            let cards: Card[] = pl.cards.concat(communityCards);
+            let primary: [PokerHand, number[]] = this.evalCards(cards);
 
+            for (let num of primary[1]) {
+                cards.splice(cards.findIndex(c => c[1] === num), 1);
+            }
 
-            evaluation.set(pl.name, this.evalCards(pl.cards.concat(communityCards)));
+            let secondary: [PokerHand, number[]] = this.evalCards(cards);
+            evaluation.set(pl, [primary, secondary]);
         }
 
-        // TODO compare evals and find winner
+        let winners: [Player, [PokerHand, number[]][]][] = [];
 
-        return [];
+        for (let [k, v] of evaluation) {
+            if (winners.length === 0) {
+                console.log(k, v);
+                winners.push([k, v]);
+            } else {
+                console.log(k, v);
+                let cmp = this.compareEvals(winners[0][1], v);
+                console.log(cmp);
+
+                if (cmp < 0) { // current won
+                    winners.length = 0;
+                    winners.push([k,v]);
+                } else if (cmp === 0) { // tie
+                    winners.push([k,v]);
+                } // cmp > 0: other won. do nothing
+            }
+        }
+
+        return winners.map(w => w[0]);
     }
 
-    private static evalCards(cards: Card[]): [PokerHand, number][] {
+    // 0: equal, >= 1: first won, <= -1: second won
+    private static compareEvals(first: [PokerHand, number[]][], second: [PokerHand, number[]][]): number {
+        let cmp = second[0][0] - first[0][0];
+
+        if (cmp === 0) {
+            cmp = Math.max(...first[0][1]) - Math.max(...second[0][1]);
+
+            if (cmp === 0) {
+                cmp = second[1][0] - first[1][0];
+
+                if (cmp === 0) {
+                    cmp = Math.max(...first[1][1]) - Math.max(...second[1][1]);
+                }
+            }
+        }
+
+        return cmp;
+    }
+
+    private static evalCards(cards: Card[]): [PokerHand, number[]] {
         cards.sort((c1, c2) => c1[1] - c2[1]);
 
-        let groupedCards: CardColor[][] = Array(15);
+        let groups: CardColor[][] = Array(15);
 
-        for (let i = 0; i < groupedCards.length; i++) {
-            groupedCards[i] = [];
+        for (let i = 0; i < groups.length; i++) {
+            groups[i] = [];
         }
 
         for (let i = 0; i < cards.length; i++) {
-            groupedCards[cards[i][1]].push(cards[i][0]);
+            groups[cards[i][1]].push(cards[i][0]);
         }
 
-        console.log(this.hasStraightFlush(groupedCards));
-
-        return [];
-    }
-
-    private static hasRoyalFlush(cards: CardColor[][]): boolean {
-        if (cards[14].length === 0) {
-            return false;
-        }
-
-        let colors: CardColor[] = cards[14];
-
-        for (let i = cards.length - 2; i >= 10; i--) {
-            let acf: boolean = false;
-
-            for (let cc of cards[i]) {
-                acf ||= colors.includes(cc);
-            }
-
-            colors = colors.filter(c => cards[i].includes(c));
-
-            if (!acf) {
-                return false;
-            }
-        }
-
-        return true;
-    }
-
-    private static hasStraightFlush(cards: CardColor[][]): [boolean, number] {
-        let colors: CardColor[] = cards[13];
-        let consecutiveCardCount: number = cards[13].length === 0 ? 0 : 1;
-        let biggestNum: number = 13;
-
-        for (let i = cards.length - 3; i >= 2; i--) {
-            if (cards[i].length !== 0) {
-                if (consecutiveCardCount === 0) {
-                    console.log(i);
-                    biggestNum = i;
-                    colors = cards[i];
-                }
-
-                if (cards[i + 1].length !== 0) {
-                    let acf: boolean = false;
-
-                    for (let cc of cards[i]) {
-                        acf ||= colors.includes(cc);
-                    }
-
-                    if (!acf) {
-                        consecutiveCardCount = 1;
-                    } else {
-                        consecutiveCardCount++;
-                        colors = colors.filter(c => cards[i].includes(c));
-
-                        if (consecutiveCardCount === 5) {
-                            break;
-                        }
-                    }
-                } else {
-                    consecutiveCardCount = 1;
-                }
-            } else {
-                consecutiveCardCount = 0;
-            }
-        }
-
-        return [consecutiveCardCount === 5, biggestNum];
-    }
-
-    private static hasFourOfAKind(cards: CardColor[][]): [boolean, number] {
-        for (let i = cards.length - 1; i >= 2; i--) {
-            if (cards[i].length === 4) {
-                return [true, i];
-            }
-        }
-
-        return [false, 0];
-    }
-
-    private static hasFlush(cards: CardColor[][]): boolean {
-        let cardColor: Map<CardColor, number> = new Map<CardColor, number>();
-        cardColor.set(CardColor.HEART, 0);
-        cardColor.set(CardColor.CLOVER, 0);
-        cardColor.set(CardColor.DIAMOND, 0);
-        cardColor.set(CardColor.SPADE, 0);
-
-        for (let i = cards.length - 1; i >= 2; i--) {
-            if (cards[i]) {
-                cards[i].forEach(color => cardColor.set(color, cardColor.get(color)! + 1));
-
-                for (let count of cardColor.values()) {
-                    if (count === 5) {
-                        return true;
-                    }
-                }
-            }
-        }
-
-        return false;
-    }
-
-    private static hasStraight(cards: CardColor[][]): [boolean, number] {
-        let consecutiveCardCount: number = cards[13].length === 0 ? 0 : 1;
-        let biggestNum: number = 13;
-
-        for (let i = cards.length - 3; i >= 2; i--) {
-            if (cards[i].length !== 0) {
-                if (consecutiveCardCount === 0) {
-                    console.log(i);
-                    biggestNum = i;
-                }
-
-                if (cards[i + 1].length !== 0) {
-                    consecutiveCardCount++;
-
-                    if (consecutiveCardCount === 5) {
-                        break;
-                    }
-                } else {
-                    consecutiveCardCount = 1;
-                }
-            } else {
-                consecutiveCardCount = 0;
-            }
-        }
-
-        return [consecutiveCardCount === 5, biggestNum];
-    }
-
-    private static hasThreeOfAKind(cards: CardColor[][]): [boolean, number] {
-        for (let i = cards.length - 1; i >= 2; i--) {
-            if (cards[i].length === 3) {
-                return [true, i];
-            }
-        }
-
-        return [false, 0];
-    }
-
-    private static hasTwoOfPair(cards: CardColor[][]): [boolean, number, number] {
-        let cardValue: number[] = [];
-        return [cards.filter((cardValues, i) => {
-            if (cardValues.length === 2) {
-                cardValue.push(i);
-                return true;
-            }
-
-            return false;
-        }).length === 2, cardValue[0], cardValue[1]];
-    }
-
-    private static hasPair(cards: CardColor[][]): [boolean, number] {
-        for (let i = cards.length - 1; i >= 2; i--) {
-            if (cards[i].length === 2) {
-                return [true, i];
-            }
-        }
-
-        return [false, 0];
-    }
-
-    private static highestCard(cards: CardColor[][]): number {
-        for (let i = cards.length - 1; i >= 2; i--) {
-            if (cards[i].length !== 0) {
-                return i;
-            }
-        }
-
-        return 0;
-    }
-
-    /*private static evalCards(cards: Card[]): [PokerHand, number][] {
-        cards.sort((c1, c2) => c1[1] - c2[1]);
-
-        let lastVal: number = cards[0][1];
-        let lastCol: string = cards[0][0];
-
-        let valEqStreak: number = 0;
-        let valIncStreak: number = 0;
-
-        let colFreqs: Map<string, number> = new Map<string, number>();
+        let consecutiveValStreak: number = 0;
 
         let pairs: number[] = [];
-        let toaks: number[] = [];
-        let foaks: number[] = [];
-        let straights: number[] = [];
-        let flushColors: string[] = [];
+        let toak: number[] = [];
+        let foak: number[] = [];
+        let straights: number[][] = [];
+        let flush: number[] | null = null;
+        let straightFlushes: number[][] = [];
+        let highCard: number = 0;
+        let colFrequencies: Map<CardColor, number[]> = new Map<CardColor, number[]>();
 
-        for (let i: number = 1; i < cards.length; i++) {
-            let currVal = cards[i][1];
-            let currCol = cards[i][0]
+        let lastCols: CardColor[] = groups[2];
 
-            valEqStreak = currVal === lastVal ? valEqStreak + 1 : 0;
-            valIncStreak = currVal === lastVal + 1 ? valIncStreak + 1 : 0;
+        for (let i = 3; i < groups.length; i++){
+            let curCols: CardColor[] = groups[i];
 
-            switch (valEqStreak) {
-                case 1:
-                    pairs.push(i - 1);
-                    break;
+            consecutiveValStreak = lastCols.length >= 1 && curCols.length >= 1 ? consecutiveValStreak + 1 : 0;
+
+            switch (curCols.length) {
                 case 2:
-                    // TODO: filtering once after completion might be more performant
-                    pairs = pairs.filter(p => p !== i - 3);
-                    toaks.push(i - 2);
+                    pairs.push(i);
                     break;
                 case 3:
-                    toaks = toaks.filter(t => t !== i - 4);
-                    foaks.push(i - 3);
+                    toak.push(i);
+                    break;
+                case 4:
+                    foak.push(i);
                     break;
             }
 
-            if (valIncStreak >= 4) {
-                straights.push(i - 4);
+            if (consecutiveValStreak >= 4) {
+                straights.push(Array.from( { length: 5 }, (_, idx) => (i - 4) + idx));
+
+                // Possibility of straight flush
+                let colors: CardColor[][] = groups.slice(i - 4, i + 1).map(c => [... new Set(c)]);
+                let results: CardColor[] = colors.reduce((a, b) => a.filter(c => b.includes(c)));
+
+                for (let res of results) {
+                    straightFlushes.push(Array.from( { length: 5 }, (_, idx) => (i - 4) + idx));
+                }
             }
 
-            colFreqs.set(currCol, (colFreqs.get(currCol) ?? 0) + 1);
-
-            lastVal = currVal;
-            lastCol = currCol;
-        }
-
-        colFreqs.forEach((val, key) => {
-            if (val >= 5) {
-                flushColors.push(key);
+            for (let col of curCols) {
+                let arr = (colFrequencies.get(col) ?? []);
+                arr.push(i);
+                colFrequencies.set(col, arr);
             }
-        });
 
-        console.log(cards);
-        console.log(pairs);
-        console.log(toaks);
-        console.log(foaks)
-        console.log(straights);
-        console.log(flushColors);
+            if (curCols.length !== 0) {
+                highCard = i;
+            }
 
-        let res: [PokerHand, number][] = [];
-
-        // Primary
-        // Royal flush. Magic number 14: value of ace, 4 offset from first card of straight to last
-        if (straights.some(s => cards[s + 4][1] === 14 && cards.slice(s, s + 5).every(c => c[0] == cards[s][0]))) {
-            console.log("rf");
-            res.push([PokerHand.ROYAL_FLUSH, 14]);
-        } else if (straights.some(s => cards.slice(s, s + 5).every(c => c[0] == cards[s][0]))) { // Straight flush
-            console.log("sf");
-            res.push([PokerHand.STRAIGHT_FLUSH, straights.filter(s => cards.slice(s, s + 5).every(c => c[0] == cards[s][0])).sort((s1, s2) => cards[s2 + 4][1] - cards[s1 + 4][1] )[0]]);
+            lastCols = curCols;
         }
 
-        console.log(res);
+        // Note: for 7 cards there can only be 1 flush so this should be fine
+        for (let [_,v] of colFrequencies.entries()) {
+            if (v.length >= 5) {
+                flush = v;
+            }
+        }
 
-        return res;
-    }*/
+        // Sort everything: They should tecnically already be in ascending order but i want descending (:
+        pairs.sort((a, b) => b - a);
+        toak.sort((a, b) => b - a);
+        foak.sort((a, b) => b - a);
+        straights.sort((s1, s2) => s2[s2.length - 1] - s1[s1.length - 1]);
+        straightFlushes.sort((s1, s2) => s2[s2.length - 1] - s1[s1.length - 1]);
+
+        let hand: [PokerHand, number[]] = [PokerHand.HIGH_CARD, [highCard]];
+        let nums: number[] | undefined;
+
+        if ((nums = straightFlushes.find(sf => sf[sf.length - 1] === 14))) {
+            hand = [PokerHand.ROYAL_FLUSH, nums];
+        } else if (straightFlushes.length !== 0) {
+            hand = [PokerHand.STRAIGHT_FLUSH, straightFlushes[0]];
+        } else if (foak.length !== 0) {
+            hand = [PokerHand.FOUR_OF_A_KIND, Array(4).fill(foak[0])];
+        } else if (toak.length >= 1 && pairs.length >= 1) {
+            hand = [PokerHand.FULL_HOUSE, Array(3).fill(toak[0]).concat(Array(2).fill(pairs[0]))];
+        } else if (flush !== null) {
+            let bestCard = Math.max(...flush);
+            flush.sort((a,b) => a - b);
+            let rem: number[] = flush.slice(0, 4);
+            hand = [PokerHand.FLUSH, [bestCard, ...rem]];
+        } else if (straights.length !== 0) {
+            hand = [PokerHand.STRAIGHT, straights[0]];
+        } else if (toak.length >= 1) {
+            hand = [PokerHand.THREE_OF_A_KIND, Array(3).fill(toak[0])];
+        } else if (pairs.length >= 2) {
+            hand = [PokerHand.TWO_PAIR, Array(2).fill(pairs[0]).concat(Array(2).fill(pairs[1]))];
+        } else if (pairs.length === 1) {
+            hand = [PokerHand.PAIR, Array(2).fill(pairs[0])];
+        }
+
+        return hand;
+    }
 }
--- poker-backend/src/index.ts
+++ poker-backend/src/index.ts
@@ -2,26 +2,10 @@ import WebSocket from "ws";
 import {Player} from "./game/player";
 import {Card, CardColor, PokerEvaluate} from "./game/card";
 
-/*const wss = new WebSocket.Server({port: 5000});
+const wss = new WebSocket.Server({port: 5000});
 
 wss.on('connection', socket => {
     socket.on('message', message => {
         socket.send(message);
     });
-});*/
-
-let p1 = new Player("keko1", 1000);
-let p2 = new Player("keko2", 1000);
-
-let cc: Card[] = [
-    [CardColor.HEART, 10],
-    [CardColor.HEART, 11],
-    [CardColor.HEART, 12],
-    [CardColor.HEART, 13],
-    [CardColor.HEART, 14]
-];
-
-p1.cards = [[CardColor.CLOVER, 6], [CardColor.CLOVER, 8]];
-p2.cards = [[CardColor.CLOVER, 2], [CardColor.DIAMOND, 3]];
-
-console.log(PokerEvaluate.evalWinners([p1], cc));
+});
